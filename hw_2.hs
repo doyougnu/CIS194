29ce60fa89e42fadd50fea1c8086953d66b8c200
doyougnu <jmy6342@gmail.com>
2016-06-03 22:30:48 +0100

    FInished exercises 2.2, 2.3, 2.4, 2.5

--- hw_2.hs
+++ hw_2.hs
@@ -36,33 +36,39 @@ exactMatches x y = sum $ map helper boollist
 -- For each peg in xs, count how many times is occurs in ys
 countColors :: Code -> [Int]
 countColors [] = []
-countColors (x:xs) = helper x:countColors xs
-  where helper z = length (filter (==z) colors)
+countColors guess = map helper colors
+  where helper z = length (filter (==z) guess)
 
 -- Count number of matches between the actual code and the guess
 matches :: Code -> Code -> Int
-matches = undefined
+matches x y = sum $ zipWith min a b
+  where a = countColors x
+        b = countColors y
 
 -- Exercise 3 -----------------------------------------
 
 -- Construct a Move from a guess given the actual code
 getMove :: Code -> Code -> Move
-getMove = undefined
+getMove x y = Move y (exactMatches x y) (matches x y - exactMatches x y)
 
 -- Exercise 4 -----------------------------------------
 
 isConsistent :: Move -> Code -> Bool
-isConsistent = undefined
+isConsistent move@(Move guess _ _) x = move == getMove x guess
 
 -- Exercise 5 -----------------------------------------
 
 filterCodes :: Move -> [Code] -> [Code]
-filterCodes = undefined
+filterCodes move = filter (isConsistent move)
 
 -- Exercise 6 -----------------------------------------
+combinations :: Int -> [a] -> [[a]] --inefficient n choose k
+combinations 0 _ = [[]]
+combinations _ [] = []
+combinations m (x:xs) = map (x:) (combinations (m-1) xs) ++ combinations m xs
 
 allCodes :: Int -> [Code]
-allCodes = undefined
+allCodes x = combinations x colors
 
 -- Exercise 7 -----------------------------------------
 
