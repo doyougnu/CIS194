4d472563db9fb65d9880bd4f82db88780c390479
doyougnu <jmy6342@gmail.com>
2016-06-06 15:41:28 -0500

    switched to 2014 class, finished 3.1

---
+++ HW3/hw3.hs
@@ -0,0 +1,21 @@
+{-# OPTIONS_GHC -Wall #-}
+module LogAnalysis where
+
+import Log
+
+---------- Exercise 1 ----------------------------------
+readAsInt :: String -> Int
+readAsInt x = read x :: Int
+
+parseMessage :: String -> MaybeLogMessage
+parseMessage str = par (words str)
+  where par (x:xs)
+          | x == "I" = ValidLM (LogMessage Info (readAsInt (head xs))
+                                (unwords $ tail xs))
+          | x == "W" = ValidLM (LogMessage Warning (readAsInt $ head xs)
+                                (unwords $ tail xs))
+          | x == "E" = ValidLM (LogMessage (Error (readAsInt (head xs)))
+                                (readAsInt $ last $ take 2 xs)
+                                (unwords $ drop 2 xs))
+          | otherwise = InvalidLM (unwords xs)
+        par [] = InvalidLM "empty input"
