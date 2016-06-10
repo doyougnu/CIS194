7c3f94d9e24bd65810d82cc2c1607d098cb6463a
doyougnu <jmy6342@gmail.com>
2016-06-10 10:31:15 -0500

    finished 4.14, ez pz

--- hw_4.hs
+++ hw_4.hs
@@ -1,10 +1,15 @@
+{-# OPTIONS_GHC -Wall #-}
+module HW04 where
+
+import Data.Char
+
 data BST a = Leaf | Node (BST a) a (BST a) deriving (Show)
  
 --skipping exercises 1-12
 ---------------------------- Exercise 13 ---------------------------------------
 insertBST :: (a -> a -> Ordering) -> a -> BST a -> BST a
-insertBST f a Leaf = Node (Leaf) a (Leaf)
-insertBST f x bst@(Node l nodeval r)
+insertBST _ a Leaf = Node (Leaf) a (Leaf)
+insertBST f x (Node l nodeval r)
   | x `f` nodeval == EQ = Node l x r
   | x `f` nodeval == LT = Node (insertBST f x l) nodeval r
   | x `f` nodeval == GT = Node l nodeval (insertBST f x r)
@@ -15,3 +20,18 @@ testBST x y
   | x > y = GT
   | x == y = EQ
 
+---------------------------- Exercise 14 ---------------------------------------
+safeHead :: [a] -> Maybe a
+safeHead [] = Nothing
+safeHead (x:_) = Just x
+
+safeTail :: [a] -> Maybe [a]
+safeTail [] = Nothing
+safeTail (_:xs) = Just xs
+
+allCaps :: [String] -> Bool
+allCaps [] = True
+allCaps xs = all hasLeadingCaps xs
+  where
+    hasLeadingCaps [] = False
+    hasLeadingCaps (y:_) = isUpper y
