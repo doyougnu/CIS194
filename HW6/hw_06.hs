59bfccb86d004d73f193a5cae6a86b8bcd532ff8
doyougnu <jmy6342@gmail.com>
2016-06-21 15:24:31 -0500

    finished 6.2, wish there were better instrucitons on testing 6.1

--- HW6/hw_06.hs
+++ HW6/hw_06.hs
@@ -5,6 +5,7 @@ module HW06 where
 import           Data.Aeson
 import           Data.Monoid
 import           GHC.Generics
+import           Data.List ( isInfixOf )
 
 import qualified Data.ByteString.Lazy.Char8 as B
 import qualified Data.Text                  as T
@@ -20,14 +21,17 @@ jsonFile = "markets.json"
 getJSON :: IO B.ByteString
 getJSON = B.readFile jsonFile
 
-what :: (T.Text, String, T.Text) -> (T.Text, String, Bool)
-what (x, String "Y") = (x, String True)
+ynToBool :: Value -> Value
+ynToBool (String "Y") = Bool True
+ynToBool (String "N") = Bool False
+ynToBool (Array v) = Array (ynToBool <$> v) -- <$> is infix fmap, translates to
+ynToBool (Object v) = Object (ynToBool <$> v) -- fmap ynToBool v
+ynToBool x = x
 
---ynToBool :: Value -> Value
+------------------------------  Exercise 2  ------------------------------------
 
-main = do
-  -- <$> is an applicative functor, just maps eitherDecode of the JSON
-  result <- (eitherDecode <$> getJSON) :: IO (Either String [Value])
-  let x = take 1 <$> result
-  return x
+parseData :: B.ByteString -> Either String Value
+parseData filedata =
+  ynToBool <$> eitherDecode filedata :: Either String Value
+
 
