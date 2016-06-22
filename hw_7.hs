c18c68057fc97816eb0c1cad418880cf2bb1defc
doyougnu <jmy6342@gmail.com>
2016-06-22 13:59:36 -0500

    finished 7.1, ez pz

--- hw_7.hs
+++ hw_7.hs
@@ -1,1 +1,10 @@
 module HW07 where
+
+------------------------------  Exercise 1  ------------------------------------
+fib :: Integer -> Integer
+fib 0 = 0
+fib 1 = 1
+fib n = fib (n - 1) + fib (n - 2)
+
+fibs :: [Integer]
+fibs = map fib [0..]
